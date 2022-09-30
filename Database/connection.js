b181ba618187e0d0c6c38fdb9d364322e49432e5
adithgit <5285@mbcpeermade.com>
2022-09-30 18:31:18 +0530

    heroku

--- Database/connection.js
+++ Database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/attendance').then(()=>{
+mongoose.connect("mongodb+srv://otto:otto@cluster0.0v04fqr.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("mongoose connected");
 });
 
